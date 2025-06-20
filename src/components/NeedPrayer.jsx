069e439d0a55d91f0b28669af7c045ac3ac7ee0d
OshaunB <oshaun1250@gmail.com>
2025-06-20 13:32:54 -0400

    changed layout for prayer page landing and some changes to mobile responsive ness, text size

--- src/components/NeedPrayer.jsx
+++ src/components/NeedPrayer.jsx
@@ -22,17 +22,18 @@ function NeedPrayer() {
         >
         <Spotlight />
         <div className="text-center relative z-3">
-          <div className="text-white text-6xl mb-8 pt-20 pb-14">
+          <div className="text-white text-3xl sm:text-6xl mb-2 pt-20 pb-10">
             Join Us for Daily Noon Prayer
+          </div>
+          <div className="text-white text-xl sm:text-2xl pt-2 mx-5">
+            Monday through Friday at 12:00 PM EST
           </div>
           <a href="https://meetings.ringcentral.com/j/8690229651" target="_blank">
             <button className="px-4 py-2 cursor-pointer rounded-xl border border-neutral-600 text-black bg-white hover:bg-gray-100 transition duration-200 my-8">
               Zoom Link
             </button>
           </a>
-          <div className="text-white text-3xl pt-18 mx-5">
-            Monday through Friday at 12:00 PM EST
-          </div>
+          
         </div>
       </ParallaxBanner>
 

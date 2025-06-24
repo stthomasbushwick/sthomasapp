b09f0d9e4a5a6ab9922f988876b6011e31fd33c4
orwrandal <randypichardo1987@gmail.com>
2025-06-23 21:46:24 -0400

    updated readings

--- src/readings.js
+++ src/readings.js
@@ -1,13 +1,13 @@
 const readings = {
     "thisSunday": {
-        verses: ["1 Kings 19:1-4, (5-7), 8-15a", "Psalm 42 and 43", "Galatians 3:23-29", "Luke 8:26-39"],
-        date:"2025-06-22",
-        name: "Second Sunday after Pentecost"
+        verses: ["2 Kings 2:1-2, 6-14", "Psalm 77:1-2, 11-20", "Galatians 5:1,13-25", "Luke 9:51-62"],
+        date:"2025-06-29",
+        name: "Third Sunday after Pentecost"
     },
     "nextSunday": {
-        verses: ["2 Kings 2:1-2, 6-14", "Psalm 77:1-2, 11-20", "Galatians 5:1,13-25", "Luke 9:51-62"],
-        date: "2025-06-29",
-        name: "Third Sunday after Pentecost"
+        verses: ["2 Kings 5:1-14", "Psalm 30", "Galatians 6:(1-6)7-16", "Luke 10:1-11, 16-20"],
+        date: "2025-07-06",
+        name: "Fourth Sunday after Pentecost"
     }
 }
 
